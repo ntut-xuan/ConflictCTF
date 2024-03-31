6363541466c943c9514e189c44f28d84bfb5331a
ntut-xuan <t109590031@ntut.org.tw>
2024-03-31 16:40:26 +0800

    Implement: Adjust implement details

--- script.py
+++ script.py
@@ -4,13 +4,13 @@ alice = []
 bob = []
 
 def make_the_list_into_char_list(list: list[str]) -> list[str]:
-    pass
+    return []
 
 def reverse_the_list(list: list[str]) -> list[str]:
-    pass
+    return []
 
 def decrease(list: list[str]) -> list[str]:
-    pass
+    return []
 
 def make_flag(alice_key, bob_key):
     for i in range(len(alice_key)):
